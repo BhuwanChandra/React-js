48376b961992990535b8cdf7b177c6038181d39e
BhuwanChandra <bhuwan7779@gmail.com>
2019-06-30 09:05:33 +0530

    created functional components

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,3 +1,21 @@
-import React from "react"
-import ReactDom from "react-dom"
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+// these are called functional components 
+function MyApp(){
+    return (
+        <div>
+            <h1>This is the Home Page</h1>
+            <ul>
+                <li>First Item</li>
+                <li>Second Item</li>
+                <li>Third Item</li>
+            </ul>
+        </div>
+    )
+}
+
+
+ReactDOM.render(<MyApp/>, document.getElementById("root"));
 
