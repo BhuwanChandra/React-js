4bc2d5cb49d094913185afdd9093a375bd4cc244
BhuwanChandra <bhuwan7779@gmail.com>
2019-07-01 15:44:57 +0530

    React Tutorial

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,17 +1,34 @@
 import React from "react";
 import TodoItem from "./components/TodoItem";
 import "./index.css";
+import todosData from "./components/todosData";
 
+/*
+
+FUNCTION BASED COMPONENT
 
 function App() {
+  const todoItems = todosData.map(todo => <TodoItem key={todo.id} todo={todo} />)
   return (
     <div className="todo-list">
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
+      {todoItems}
     </div>
    )
 };
+
+*/
+
+// CLASS BASED COMPONENT
+
+class App extends React.Component {
+  render(){
+    const todoItems = todosData.map(todo => <TodoItem key={todo.id} todo={todo} />)
+    return (
+      <div className="todo-list">
+        {todoItems}
+      </div>
+    )
+  }
+}
  
 export default App;
--- todo-app/src/components/TodoItem.js
+++ todo-app/src/components/TodoItem.js
@@ -1,14 +1,32 @@
 import React from "react";
 import "../index.css";
 
-function TodoItem() {
+/*
+
+function TodoItem(props) {
     return (
         <div className="todo-item">
             <p>
-                <input type="checkbox" ></input> <span>first Todo</span>
+                <input type="checkbox" checked={props.todo.completed}></input>
+                <span>{props.todo.text}</span>
             </p>
         </div>
     )
 };
+
+*/
+
+class TodoItem extends React.Component{
+    render(){
+        return (
+            <div className="todo-item">
+                <p>
+                    <input type="checkbox" checked={this.props.todo.completed}></input>
+                    <span>{this.props.todo.text}</span>
+                </p>
+            </div>
+        ) 
+    }
+}
 
 export default TodoItem;
