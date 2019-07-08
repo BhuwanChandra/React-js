de9d58271d92decfac595693c7e4e92875301a69
BhuwanChandra <bhuwan7779@gmail.com>
2019-07-08 14:51:05 +0530

    created u tube video fetching app

---
+++ utube/src/Youtube.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+
+const API = "AIzaSyAlsQ1NWg6I53719SFacNUILu7BGf-SGMo";
+const channelID = "UCXgGY0wkgOzynnHvSEVmE3A";
+const result = 20;
+
+var finalURL = `https://www.googleapis.com/youtube/v3/search?key=${API}&channelId=${channelID}&part=snippet,id&order=date&maxResults=${result}`;
+
+class Youtube extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            resultyt: []
+        }
+        this.clicked = this.clicked.bind(this);
+    }
+
+    clicked(){
+        fetch(finalURL)
+            .then((response) => response.json())
+            .then((responseJson) => {
+                // console.log(responseJson);
+                const resultyt = responseJson.items.map(obj => "https://www.youtube.com/embed/" + obj.id.videoId);
+                this.setState({resultyt});
+            })
+            .catch((error) => {
+                console.error(error);
+        });
+    }
+
+  render() {
+    // Api key = AIzaSyAlsQ1NWg6I53719SFacNUILu7BGf-SGMo
+    // https://www.youtube.com/channel/UCXgGY0wkgOzynnHvSEVmE3A
+      console.log(finalURL);
+      console.log(this.state.resultyt);
+    return (
+      <div>
+        <button onClick={this.clicked}>Get Youtube Videos</button>
+            {
+                this.state.resultyt.map((link, i) => {
+                    console.log(link);
+                    var frame =
+                        <div className="youtube" key={i}><iframe
+                        title="Youtube React App"
+                        width="500"
+                        height="280"
+                        src={link}
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                    /></div>
+                    return frame;
+                })
+            }
+            {this.frame}
+      </div>
+    );
+  }
+}
+
+export default Youtube;
+
+
+// https://www.googleapis.com/youtube/v3/search?key=AIzaSyAlsQ1NWg6I53719SFacNUILu7BGf-SGMo&channelId=UCXgGY0wkgOzynnHvSEVmE3A&part=snippet,id&order=date&maxResults=10
