59d0236bf0192299fb17d04f9dacc711f1f6513f
BhuwanChandra <bhuwan7779@gmail.com>
2020-03-13 18:45:43 +0530

    added react-redux-demo app

---
+++ react-redux-demo/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import CakeComponent from './components/CakeComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <CakeComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-redux-demo/src/components/CakeComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function CakeComponent() {
+    return (
+        <div>
+            <h2>Number of Cakes</h2>
+            <button>Buy Cakes</button>
+        </div>
+    )
+}
+
+export default CakeComponent
