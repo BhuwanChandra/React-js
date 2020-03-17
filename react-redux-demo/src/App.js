f16f6d34b76ba04b14ae0a22cd93f6a20ce212bc
BhuwanChandra <bhuwan7779@gmail.com>
2020-03-17 12:34:21 +0530

    added cakeComponent with hooks

--- react-redux-demo/src/App.js
+++ react-redux-demo/src/App.js
@@ -3,12 +3,16 @@ import { Provider } from 'react-redux'
 import store from './redux/store'
 import './App.css';
 import CakeComponent from './components/CakeComponent';
+import IceCreamComponent from './components/IceCreamComponent';
+import HooksCakeComponent from './components/HooksCakeComponent';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
         <CakeComponent />
+        <IceCreamComponent />
+        <HooksCakeComponent />
       </div>
     </Provider>
   );
